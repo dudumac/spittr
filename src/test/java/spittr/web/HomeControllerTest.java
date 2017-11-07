f5e8b6d8c23bcff028742d857ad581fabbf62d62
cormac <cormac@cormac-pc.localnet>
2017-11-07 13:55:36 +0000

    Refactor HomeController to use class-level RequestMapping

--- src/test/java/spittr/web/HomeControllerTest.java
+++ src/test/java/spittr/web/HomeControllerTest.java
@@ -10,10 +10,18 @@ import org.springframework.test.web.servlet.MockMvc;
 public class HomeControllerTest {
 
 	@Test
-	public void testHomePage() throws Exception {
+	public void testDefaultRoute() throws Exception {
 		HomeController controller = new HomeController();
 		MockMvc mockMvc = standaloneSetup(controller).build();
 		
 		mockMvc.perform(get("/")).andExpect(view().name("home"));
+	}
+
+	@Test
+	public void testHomePage() throws Exception {
+		HomeController controller = new HomeController();
+		MockMvc mockMvc = standaloneSetup(controller).build();
+		
+		mockMvc.perform(get("/homepage")).andExpect(view().name("home"));
 	}
 }
