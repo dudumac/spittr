5f6f7da30b737a7e40fb7ec98a2045e2784e2d99
cormac <cormac@cormac-pc.localnet>
2017-11-07 15:23:32 +0000

    - Add test for Spittle

--- src/main/java/spittr/web/SpittleController.java
+++ src/main/java/spittr/web/SpittleController.java
@@ -1,17 +1,23 @@
 package spittr.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import spittr.Spittle;
 import spittr.data.SpittleRepository;
 
 @Controller
 @RequestMapping("/spittles")
 public class SpittleController {
 
+	private static final String MAX_LONG_AS_STRING = "" + Long.MAX_VALUE;
 	private SpittleRepository spittleRepository;
 
 	@Autowired
@@ -19,9 +25,34 @@ public class SpittleController {
 		this.spittleRepository = spittleRepository;
 	}
 
+	/*
+	 * Alternate, explicit implementation - Update the model with the relevant
+	 * attributes and return the name of the view used to render the updated
+	 * model
+	 * 
+	 * @RequestMapping(method = RequestMethod.GET) 
+	 * public String spittles(
+	 * 		@RequestParam(value = "max", defaultValue = MAX_LONG_AS_STRING) long max,
+	 * 		@RequestParam(value = "count", defaultValue = "20") int count, Model model) {
+	 * 		model.addAttribute(spittleRepository.findSpittles(max, count));
+	 * 		return "spittles";
+	 *  }
+	 */
+
+	/*
+	 * Implicit implementaiton, view name will be inferred from Request Path
+	 * (i.e. spittles) and retruned value will be added to model (model
+	 * attribute name inferred from return type spittleList)
+	 */
 	@RequestMapping(method = RequestMethod.GET)
-	public String spittles(Model model) {
-		model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
-		return "spittles";
+	public List<Spittle> spittles(@RequestParam(value = "max", defaultValue = MAX_LONG_AS_STRING) long max,
+			@RequestParam(value = "count", defaultValue = "20") int count, Model model) {
+		return spittleRepository.findSpittles(max, count);
+	}
+
+	@RequestMapping(value = "/{spittleId}", method = RequestMethod.GET)
+	public String spittle(@PathVariable("spittleId") long spittleId, Model model) {
+		model.addAttribute(spittleRepository.findOne(spittleId));
+		return "spittle";
 	}
 }
--- src/test/java/spittr/web/SpittleControllerTest.java
+++ src/test/java/spittr/web/SpittleControllerTest.java
@@ -40,6 +40,43 @@ public class SpittleControllerTest {
 
 	}
 
+	@Test
+	public void shouldShowPagedSpittles() throws Exception {
+		List<Spittle> expectedSpittles = createSpittleList(50);
+
+		SpittleRepository mockRepository = mock(SpittleRepository.class);
+		when(mockRepository.findSpittles(238900, 50)).thenReturn(expectedSpittles);
+
+		SpittleController controller = new SpittleController(mockRepository);
+		MockMvc mockMvc = standaloneSetup(controller)
+				.setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp"))
+				.build();
+		mockMvc.perform(get("/spittles?max=238900&count=50"))
+				.andExpect(view().name("spittles"))
+				.andExpect(model().attributeExists("spittleList"))
+				.andExpect(model().attribute("spittleList", hasItems(expectedSpittles.toArray())));
+	}
+
+	/*
+	 * As a general rule, query parameters should not be used to identify a
+	 * resource. A GET request for /spittles/12345 is better than one for
+	 * /spittles/show?spittle_id=12345.
+	 */
+	@Test
+	public void testSpittle() throws Exception {
+		Spittle expectedSpittle = new Spittle("Hello", new Date());
+		
+		SpittleRepository mockRepository = mock(SpittleRepository.class);
+		when(mockRepository.findOne(12345)).thenReturn(expectedSpittle);
+		
+		SpittleController controller = new SpittleController(mockRepository);
+		MockMvc mockMvc = standaloneSetup(controller).build();
+		mockMvc.perform(get("/spittles/12345"))
+				.andExpect(view().name("spittle"))
+				.andExpect(model().attributeExists("spittle"))
+				.andExpect(model().attribute("spittle", expectedSpittle));
+	}
+
 	private List<Spittle> createSpittleList(int count) {
 		List<Spittle> spittles = new ArrayList<Spittle>();
 		for (int i = 0; i < count; i++) {
